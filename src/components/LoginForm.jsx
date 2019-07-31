01a55293df02c8b0936ed1d534ecece751ae87ce
Gabriel Buchdahl <gbuchdahl@gmail.com>
2019-07-30 23:40:45 -0400

    changed formatting on loginform

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
-import { Form, Button } from 'react-bootstrap'
-
-
+import { Form, Button, Container, Row } from "react-bootstrap";
 
 class LoginForm extends Component {
   state = {
@@ -11,14 +9,14 @@ class LoginForm extends Component {
     password: "",
     student: true,
     registration: true
-  }
+  };
 
   constructor() {
-    super()
-    this.handleUserInfoChange = this.handleUserInfoChange.bind(this)
-    this.handleRegistration = this.handleRegistration.bind(this)
-    this.handleButtonSwitch = this.handleButtonSwitch.bind(this)
-    this.handleUserSwitch = this.handleUserSwitch.bind(this)
+    super();
+    this.handleUserInfoChange = this.handleUserInfoChange.bind(this);
+    this.handleRegistration = this.handleRegistration.bind(this);
+    this.handleButtonSwitch = this.handleButtonSwitch.bind(this);
+    this.handleUserSwitch = this.handleUserSwitch.bind(this);
   }
 
   handleUserInfoChange(event) {
@@ -27,19 +25,21 @@ class LoginForm extends Component {
         [event.target.name]: event.target.value,
         student: prevState.student,
         registration: prevState.registration
-      }
-    })
+      };
+    });
   }
 
   handleRegistration() {
-    let username = this.state.username
-    let password = this.state.password
+    let username = this.state.username;
+    let password = this.state.password;
 
-    firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-    });
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(username, password)
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
   }
 
   handleButtonSwitch() {
@@ -49,8 +49,8 @@ class LoginForm extends Component {
         password: prevState.password,
         student: prevState.student,
         registration: !prevState.registration
-      }
-    })
+      };
+    });
   }
 
   handleUserSwitch() {
@@ -60,51 +60,75 @@ class LoginForm extends Component {
         password: prevState.password,
         student: !prevState.student,
         registration: prevState.registration
-      }
-    })
+      };
+    });
   }
 
   render() {
-    return (<div>
-      <h3 className="text-center">{
-          this.state.student
-            ? "Find your externship today!"
-            : "Find motivated STEM students from top schools!"
-        }</h3>
+    return (
+      <Container>
+        <Row>
+          <h3 className="text-center">
+            {this.state.student
+              ? "Find your externship today!"
+              : "Find motivated STEM students from top schools!"}
+          </h3>
+        </Row>
 
-      <br/>
+        <Row>
+          <Form>
+            <Form.Group controlId="form_username">
+              <Form.Control
+                onChange={this.handleUserInfoChange}
+                name="username"
+                type="text"
+                placeholder="Username"
+              />
+            </Form.Group>
 
-      <Form>
-        <Form.Group controlId="form_username">
-          <Form.Control onChange={this.handleUserInfoChange} name="username" type="text" placeholder="Username" />
-        </Form.Group>
+            <Form.Group controlId="form_password">
+              <Form.Control
+                onChange={this.handleUserInfoChange}
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
 
-        <Form.Group controlId="form_password">
-          <Form.Control onChange={this.handleUserInfoChange} name="password" type="password" placeholder="Password" />
-        </Form.Group> 
-        
-        <Button variant="primary" type="submit" onClick={this.handleRegistration}>
-          { this.state.registration
-            ? "Register"
-            : "Sign In"
-          }
-        </Button>
-      </Form>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={this.handleRegistration}
+            >
+              {this.state.registration ? "Register" : "Sign In"}
+            </Button>
+          </Form>
+        </Row>
 
-        <Button className="btn btn-light btn-sm" onClick={this.handleButtonSwitch}>{
-            this.state.registration
+        <Row>
+          <Button
+            className="btn btn-light btn-sm"
+            onClick={this.handleButtonSwitch}
+          >
+            {this.state.registration
               ? "Have an account?  Sign In."
-              : "Don't have an account? Create one now!"
-          }</Button>
+              : "Don't have an account? Create one now!"}
+          </Button>
+        </Row>
+
+        <Row>
+          <Button
+            className="btn btn-light btn-sm"
+            onClick={this.handleUserSwitch}
+          >
+            {this.state.student
+              ? "Looking for employees?"
+              : "Are you a current student?"}
+          </Button>
+        </Row>
 
         <br />
-        <Button className="btn btn-light btn-sm" onClick={this.handleUserSwitch}>{
-            this.state.student
-              ? "Looking for employees?"
-              : "Are you a current student?" }
-        </Button>
-        
-      </div>
+      </Container>
     );
   }
 }
