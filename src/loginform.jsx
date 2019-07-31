abf386ecc93ff9fc6b362ec5eab689672c2af0c3
Jordan Epstein <32082339+jordepic@users.noreply.github.com>
2019-07-30 21:13:05 -0400

    Changing Authentication Options
    Accounts for companies, registration/sign in

--- src/loginform.jsx
+++ src/loginform.jsx
@@ -7,53 +7,114 @@ import "firebase/auth";
 class LoginForm extends Component {
   state = {
     username: "",
-    password: ""
+    password: "",
+    student: true,
+    registration: true
   }
 
   constructor() {
     super()
-    this.handleChange = this.handleChange.bind(this)
+    this.handleUserInfoChange = this.handleUserInfoChange.bind(this)
     this.handleRegistration = this.handleRegistration.bind(this)
+    this.handleButtonSwitch = this.handleButtonSwitch.bind(this)
+    this.handleUserSwitch = this.handleUserSwitch.bind(this)
   }
 
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
+  handleUserInfoChange(event) {
+    this.setState(prevState => {
+      return {
+        [event.target.name]: event.target.value,
+        student: prevState.student,
+        registration: prevState.registration
+      }
     })
   }
 
   handleRegistration() {
     let username = this.state.username
     let password = this.state.password
-    console.log(username)
-    console.log(password)
+
     firebase.auth().createUserWithEmailAndPassword(username, password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log(errorMessage)
+
     });
+  }
+
+  handleButtonSwitch() {
+    this.setState(prevState => {
+      return {
+        username: prevState.username,
+        password: prevState.password,
+        student: prevState.student,
+        registration: !prevState.registration
+      }
+    })
+  }
+
+  handleUserSwitch() {
+    this.setState(prevState => {
+      return {
+        username: prevState.username,
+        password: prevState.password,
+        student: !prevState.student,
+        registration: prevState.registration
+      }
+    })
   }
 
   render() {
     return (<div>
       <Navbar/>
-      <h3 className="text-center">Create an account for available externships!</h3>
+
+      <br/>
+
+      <h3 className="text-center">{
+          this.state.student
+            ? "Find your externship today!"
+            : "Find motivated STEM students from top schools!"
+        }</h3>
+
+      <br/>
 
       <form>
         <div className="form-group">
-          <input type="text" name="username" placeholder="Username" className="form-control" onChange={this.handleChange}/>
+          <input type="text" name="username" placeholder="Username" className="form-control" onChange={this.handleUserInfoChange}/>
         </div>
 
         <div className="form-group">
-          <input type="text" name="password" placeholder="Password" className="form-control" onChange={this.handleChange}/>
+          <input type="text" name="password" placeholder="Password" className="form-control" onChange={this.handleUserInfoChange}/>
         </div>
 
         <br/>
         <div className="text-center">
-          <button className="btn btn-dark btn-lg" onClick={this.handleRegistration}>Register</button>
+          <button className="btn btn-dark btn-lg" onClick={this.handleRegistration}>{
+              this.state.registration
+                ? "Register"
+                : "Sign In"
+            }</button>
         </div>
-
       </form>
+
+      <br/>
+
+      <div className="text-center">
+        <button className="btn btn-light btn-sm" onClick={this.handleButtonSwitch}>{
+            this.state.registration
+              ? "Have an account?  Sign In."
+              : "Don't have an account? Create one now!"
+          }</button>
+      </div>
+
+      <br/>
+
+      <div className="text-center">
+        <button className="btn btn-light btn-sm" onClick={this.handleUserSwitch}>{
+            this.state.student
+              ? "Looking for employees?"
+              : "Are you a current student?"
+          }</button>
+      </div>
     </div>);
   }
 }
